b5f43a5aa8974525bf24338aaa05cf84962a954b
Alex <alexboschmateu2007@gmail.com>
2023-10-16 18:06:16 +0200

    v1.1

--- preguntesrandoms.py
+++ preguntesrandoms.py
@@ -1,8 +1,13 @@
 import random
 
-with open('C:\\Users\\bosch\\Desktop\\Preguntes random\\log.txt', 'a', encoding='utf-8') as c:     #crea els 2 arxius si no estan
+with open('log.txt', 'w', encoding='utf-8') as b:   # esborra els dos arxius si estan presents
+    pass  
+with open('respostes.txt', 'w', encoding='utf-8') as a:
+    pass  
+
+with open('log.txt', 'a', encoding='utf-8') as c:     #crea els 2 arxius si no estan
     pass
-with open('C:\\Users\\bosch\\Desktop\\Preguntes random\\respostes.txt', 'a', encoding='utf-8') as v:
+with open('respostes.txt', 'a', encoding='utf-8') as v:
     pass
 
 file = open('preguntes.txt', 'r', encoding='utf-8')  # obrir arxiu
@@ -12,14 +17,14 @@ lines = file.readlines()                                # assignar lineas a la v
 
 def printandlog(s):           #la s es refereix a la variable random line mes endevant/ funció per guardar i per escriure
     print(s)
-    with open('C:\\Users\\bosch\\Desktop\\Preguntes random\\log.txt', 'a', encoding='utf-8') as f: #obrir document log per escriure tmb/ la n fica un espai extra??? 
+    with open('log.txt', 'a', encoding='utf-8') as f: #obrir document log per escriure tmb/ la n fica un espai extra??? 
         f.write(s + '\n') # / la s es torna a referir a la linea random de mes endevant, pertant escriu la linea random + el espai...........^^
     x = input('Resposta:')
-    with open('C:\\Users\\bosch\\Desktop\\Preguntes random\\respostes.txt', 'a', encoding='utf-8') as k:
+    with open('respostes.txt', 'a', encoding='utf-8') as k:
         k.write(x + '\n')
 
 while True:  #mentres quedin preguntes NO a log
-    with open('C:\\Users\\bosch\\Desktop\\Preguntes random\\log.txt', 'r', encoding='utf-8') as f:           # assigna el document a la variable logdoc per compararles
+    with open('log.txt', 'r', encoding='utf-8') as f:           # assigna el document a la variable logdoc per compararles
         logdoc = f.read()
 
     lines_not_in_logdoc = [line for line in lines if line not in logdoc]   # crea la funcio que compara les lines a logdoc amb les de preguntes
@@ -31,8 +36,4 @@ while True:  #mentres quedin preguntes NO a log
     
     if not lines_not_in_logdoc:      # si totes les lineas de log doc (log.txt) estan a preguntes.txt escriu JA ESTA   log = preguntes   
         print('No queden més preguntes')
-        with open('C:\\Users\\bosch\\Desktop\\Preguntes random\\log.txt', 'w', encoding='utf-8') as b:
-            pass  
-        with open('C:\\Users\\bosch\\Desktop\\Preguntes random\\respostes.txt', 'w', encoding='utf-8') as a:
-            pass  
         break
